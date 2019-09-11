235506fa8744f3a682bf0f17536b0387c59d0618
Hunter <haeilmes@gmail.com>
2019-09-11 13:52:44 -0500

    Second Commit

--- weeklyChallenges03/ChallengesSet03.cs
+++ weeklyChallenges03/ChallengesSet03.cs
@@ -47,7 +47,11 @@ namespace weeklyChallenges03
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if(divisor == 0)
+            {
+                return 0;
+            }
+            return dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
